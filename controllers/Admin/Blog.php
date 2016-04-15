35771211db7a6608f9f92bdea0603d24b2f0634e
mirzapulatov <mirzapulatov@gmail.com>
2016-04-15 10:08:09 +0300

    Fixed Bug 2

--- controllers/Admin/Blog.php
+++ controllers/Admin/Blog.php
@@ -22,10 +22,10 @@ if ($protect) {
                     $msg = 'Ошибка имени. Имя должно состоять из латинских символов, а так же в диапазоне от 3 до 20 символов. <br/>';
                 }
                 if (!$checker->stringLength($_POST['title'], 20, 300)) {
-                    $msg = sprintf('%s Ошибка заголовка.Заголовок должен состоять из 20-300 символов. <br/>', Smsg);
+                    $msg = sprintf('%s Ошибка заголовка.Заголовок должен состоять из 20-300 символов. <br/>', $msg);
                 }
                 if (!$checker->stringLength($_POST['text'], 100, 5000)) {
-                    $msg = sprintf('%s Ошибка сообщения. Сообщение должно состоять из 100-5000 символов. <br/>', Smsg);
+                    $msg = sprintf('%s Ошибка сообщения. Сообщение должно состоять из 100-5000 символов. <br/>', $msg);
                 }
                 if (empty($msg)) {
                     $blogModel->create(array('title','author','text','time'), array($_POST['title'], $_POST['author'], $_POST['text'], time()));
@@ -44,10 +44,10 @@ if ($protect) {
                     $msg = 'Ошибка имени. Имя должно состоять из латинских символов, а так же в диапазоне от 3 до 20 символов. <br/>';
                 }
                 if (!$checker->stringLength($_POST['title'], 20, 300)) {
-                    $msg = sprintf('%s Ошибка заголовка.Заголовок должен состоять из 20-300 символов. <br/>', Smsg);
+                    $msg = sprintf('%s Ошибка заголовка.Заголовок должен состоять из 20-300 символов. <br/>', $msg);
                 }
                 if (!$checker->stringLength($_POST['text'], 100, 5000)) {
-                    $msg = sprintf('%s Ошибка сообщения. Сообщение должно состоять из 100-5000 символов. <br/>', Smsg);
+                    $msg = sprintf('%s Ошибка сообщения. Сообщение должно состоять из 100-5000 символов. <br/>', $msg);
                 }
                 if (empty($msg)) {
                     $blogModel->update(array('title','author','text','time'), array($_POST['title'], $_POST['author'], $_POST['text'], time()), 'id = '.$id);
